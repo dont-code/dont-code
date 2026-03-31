885abddc26e8f45dd451e08eabb57ebdbbe98ef3
Gérard Collin <gerard.collin@appliweb.net>
2026-03-31 15:46:42 +0200

    fix: enable direct move to generate

--- apps/dashboard/src/app/app.ts
+++ apps/dashboard/src/app/app.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject} from '@angular/core';
+import {Router, RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,24 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
+  protected router=inject(Router);
   protected title = 'dashboard';
+
+  constructor() {
+    const config = (window as any).dashboardConfig;
+
+    const action=config.action;
+    console.log(action);
+    if (action!=null) {
+      this.runAction(action);
+    }
+  }
+
+  runAction (actionName:string) {
+    if (actionName == 'generate') {
+      return this.router.navigate(['generate','default']);
+    } else {
+      return Promise.reject("No action named "+actionName);
+    }
+  }
 }
